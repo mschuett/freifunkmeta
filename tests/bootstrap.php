7ef4f2d1b8582314b8d50a4ea3308c77d7746146
Martin Schütte <martin.schuette@deck36.de>
2014-06-16 16:45:59 +0200

    need no vendors for testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,5 +12,4 @@ tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
 
-require_once 'vendor/autoload.php';
 require 'mock_ext_dataservice.php';
